bfcb5252c8dab863303065c4a1f3433b9a9ed75d
Thomas van Boom <Tvanboom@dis-sensors.nl>
2023-04-11 13:53:41 +0200

    first commit

---
+++ Main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Pls work'
+
+if __name__ == '__main__':
+    # Run the app when the program starts!
+    app.run(debug=True)
---
+++ Main_test_file.py
@@ -0,0 +1,7 @@
+from Main import app
+
+def test_index():
+    response = app.test_client().get('/')
+    
+    assert response.data == b'Pls work'
+    assert response.status_code == 200
