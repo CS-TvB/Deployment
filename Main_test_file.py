65f1dce35e29b4edcb5460d7346c2b069579a3c1
Thomas van Boom <Tvanboom@dis-sensors.nl>
2023-04-20 15:16:32 +0200

    .. gone

--- Main_test_file.py
+++ Main_test_file.py
@@ -3,5 +3,5 @@ from Main import app
 def test_index():
     response = app.test_client().get('/')
     
-    assert response.data == b'Pls work my dude....'
+    assert response.data == b'Pls work my dude'
     assert response.status_code == 200
