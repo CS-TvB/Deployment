4af268db34c124ef244790f7ca43d0fb7e34aa39
Thomas van Boom <Tvanboom@dis-sensors.nl>
2023-05-02 12:01:22 +0200

    ye

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Pls work......## it works now?'
+    return 'Pls work......## it works now?, ye it does'
 
 if __name__ == '__main__':
     # Run the app when the program starts!
