b65a4797edcfed28ac276f9c72afcc82af5e676b
Thomas van Boom <Tvanboom@dis-sensors.nl>
2023-05-02 16:38:14 +0200

    now it all works while running :)

--- Main_test_file.py
+++ Main_test_file.py
@@ -3,5 +3,5 @@ from Main import app
 def test_index():
     response = app.test_client().get('/')
     
-    assert response.data == b'Pls work..'
+    assert response.data == b'It finaly works jaaayy!!'
     assert response.status_code == 200
